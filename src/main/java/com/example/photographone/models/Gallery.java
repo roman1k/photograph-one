ca43cde47dd27f7688440f7fc7f1ecd7c5c468c0
koval98 <samborskiy1998@gmail.com>
2018-11-21 14:22:44 +0200

    Photo i Gallery

--- src/main/java/com/example/photographone/models/Gallery.java
+++ src/main/java/com/example/photographone/models/Gallery.java
@@ -16,6 +16,48 @@ public class Gallery {
     @OneToMany
     private List<Photo> photos = new ArrayList<Photo>();
 
+    public Gallery(){}
 
+    public Gallery(String nameGallery, Photograph photograph, List<Photo> photos) {
+        this.nameGallery = nameGallery;
+        this.photograph = photograph;
+        this.photos = photos;
+    }
+
+
+
+    public void setNameGallery(String nameGallery) {
+        this.nameGallery = nameGallery;
+    }
+
+    public void setPhotograph(Photograph photograph) {
+        this.photograph = photograph;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    @Override
+    public String toString() {
+        return "Gallery{" +
+                "id=" + id +
+                ", nameGallery='" + nameGallery + '\'' +
+                ", photograph=" + photograph +
+                ", photos=" + photos +
+                '}';
+    }
+
+    public String getNameGallery() {
+        return nameGallery;
+    }
+
+    public Photograph getPhotograph() {
+        return photograph;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
 
 }
