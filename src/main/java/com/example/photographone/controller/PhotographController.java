582c257e8a4187269eb4c70c4a0fe215fa0b3617
koval98 <samborskiy1998@gmail.com>
2018-11-21 14:52:09 +0200

    cтворив шаблон для збереженя даних фотографа

---
+++ src/main/java/com/example/photographone/controller/PhotographController.java
@@ -0,0 +1,21 @@
+package com.example.photographone.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PhotographController {
+
+    @PostMapping("/saveFirstPhotographer")
+    public String savefirstphotograph(
+
+    ){
+
+    }
+
+
+
+
+
+}
