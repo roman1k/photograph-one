eff25c3fb315a8bd8c21e5068b32f137a17654dc
roman1k <roman1_k@ukr.net>
2018-11-21 14:43:37 +0200

    update City, Contact, Costumer, UserDepended

--- src/main/java/com/example/photographone/DAO/ContactDAO.java
+++ src/main/java/com/example/photographone/DAO/ContactDAO.java
@@ -3,7 +3,10 @@ package com.example.photographone.DAO;
 import com.example.photographone.models.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ContactDAO  extends JpaRepository<Contact,Integer> {
+    List<Contact> findByCity_NameCity(String nameCity);
 
 }
--- src/main/java/com/example/photographone/models/City.java
+++ src/main/java/com/example/photographone/models/City.java
@@ -1,7 +1,45 @@
 package com.example.photographone.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nameCity;
+    @OneToMany
+    private List<Contact> contacts;
+//constructor
+    public City() {
+    }
+// getter and setter
+    public int getId() {
+        return id;
+    }
+
+    public String getNameCity() {
+        return nameCity;
+    }
+
+    public void setNameCity(String nameCity) {
+        this.nameCity = nameCity;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", contacts=" + contacts +
+                '}';
+    }
 }
