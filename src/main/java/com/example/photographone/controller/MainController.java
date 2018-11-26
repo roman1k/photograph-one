62d30983bbc408ce6078e27c0e6bd3912f2fdf6e
koval98 <samborskiy1998@gmail.com>
2018-11-26 13:12:56 +0200

    зробив фотограф сервіс

--- src/main/java/com/example/photographone/controller/MainController.java
+++ src/main/java/com/example/photographone/controller/MainController.java
@@ -44,7 +44,6 @@ public class MainController {
 
     @PostMapping("/successURL")
     private  String saveUser( ) {
-
         return "next";
     }
 }
--- src/main/java/com/example/photographone/controller/PhotographController.java
+++ src/main/java/com/example/photographone/controller/PhotographController.java
@@ -6,8 +6,11 @@ import com.example.photographone.DAO.UserDAO;
 import com.example.photographone.models.Photograph;
 import com.example.photographone.models.Sex;
 import com.example.photographone.models.User;
+import com.example.photographone.models.UserDepended;
 import com.example.photographone.service.PhotographService;
 import com.example.photographone.service.implem.PhotographServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -19,8 +22,12 @@ import java.security.Principal;
 
 @Controller
 public class PhotographController {
-    PhotographDAO photographDAO;
-    UserDAO userDAO;
+    @Autowired
+   private PhotographDAO photographDAO;
+    @Autowired
+   private UserDAO userDAO;
+    @Qualifier("photographServiceImpl")
+   private PhotographService photographService;
 
     @PostMapping("/saveFirstPhotographer")
     public String savefirstphotograph(
@@ -38,22 +45,20 @@ public class PhotographController {
             @RequestParam int Sale,
             @RequestParam String nameGallery,
             @RequestParam String photos,
-            @AuthenticationPrincipal
-            ){
+            @AuthenticationPrincipal UserDepended userDepended
+            ) {
+        int id_photograph = userDepended.getId();
+        Photograph photograph = photographDAO.getOne(id_photograph);
+        Photograph photograph1 = photographService.getPhotograph(avatar.getOriginalFilename(),
+                firstName, lastName, Age, sex, description, email, number, facebook,
+                instagram, city, Sale, nameGallery, photos, photograph);
 
-            userDAO.getOne(userDetails.);
-            Photograph photograph =  userDAO.findByUsername(userDetails.getUsername());
-            photograph.get
-          //  principal.getName();
-
-        // .getOne(userDAO.findByUsername(principal.getName()));  // userDAO.findByUsername(principal.getName());
-
-
-
-
-
-
+        photographService.save(photograph1);
+        Photograph photograp;
+        return "index";
     }
+
+
 
 
 
