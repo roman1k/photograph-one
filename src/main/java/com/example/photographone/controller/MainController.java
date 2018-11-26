1049e3adcb5e09605b4a70c64a4377b06d6cca7a
koval98 <samborskiy1998@gmail.com>
2018-11-26 14:31:04 +0200

    зробив фотограф сервіс

--- src/main/java/com/example/photographone/controller/MainController.java
+++ src/main/java/com/example/photographone/controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.URLEncoder;
@@ -46,4 +47,7 @@ public class MainController {
     private  String saveUser( ) {
         return "next";
     }
+
+
+
 }
