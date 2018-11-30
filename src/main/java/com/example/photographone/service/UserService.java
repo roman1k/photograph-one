72c3ca88cf46a3c54d441570e31f9c5c0888546a
nataliaMykhailova <natalia.mykhailova.95@gmail.com>
2018-11-30 15:44:05 +0200

    ==

--- src/main/java/com/example/photographone/service/UserService.java
+++ src/main/java/com/example/photographone/service/UserService.java
@@ -1,7 +1,8 @@
 package com.example.photographone.service;
 
 import com.example.photographone.models.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     void save(User user);
 }
