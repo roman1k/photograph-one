406c4416b5f96b2cc09c9fef3214aa4627f9fc1f
roman1k <roman1_k@ukr.net>
2018-12-04 15:12:23 +0200

    create service for Costumer

---
+++ src/main/java/com/example/photographone/service/implem/CostumerServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.photographone.service.implem;
+
+import com.example.photographone.service.CostumerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CostumerServiceImpl implements CostumerService {
+
+
+}
