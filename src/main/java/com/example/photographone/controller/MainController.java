2970825d0be7ccaa56484e23cb450492ea7f0429
roman1k <roman1_k@ukr.net>
2018-12-06 15:01:40 +0200

    Add to MainController

--- src/main/java/com/example/photographone/controller/MainController.java
+++ src/main/java/com/example/photographone/controller/MainController.java
@@ -3,17 +3,16 @@ package com.example.photographone.controller;
 import com.example.photographone.DAO.ContactDAO;
 import com.example.photographone.DAO.CostumerDAO;
 import com.example.photographone.DAO.UserDAO;
-import com.example.photographone.models.Contact;
-import com.example.photographone.models.Costumer;
-import com.example.photographone.models.Role;
-import com.example.photographone.models.User;
+import com.example.photographone.models.*;
 import com.example.photographone.service.UserService;
 import com.example.photographone.service.implem.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.URLEncoder;
@@ -25,23 +24,34 @@ public class MainController {
 
 
 
-    @GetMapping("/newPhotograph")
+    @PostMapping("/newPhotograph")
     public String newPhotograph(User user, Contact contact){
         userService.savePhotograph(user,contact);
         return "redirect:/";
     }
 
-    @GetMapping("/newUser")
+    @PostMapping("/newUser")
     public String newUser(User user, Contact contact){
         userService.saveUser(user,contact);
         return "redirect:/";
     }
 
     @PostMapping("/successURL")
-    private  String saveUser() {
+    private  String saveUser(@AuthenticationPrincipal User user) {
         System.out.println("YOU+++++++++++++++++++++++++++++++++++++++");
-        return "redirect:/photographProfile";
+        return "redirect:/photographProfile/"+ user.getUsername();
     }
+    @PostMapping("/photographProfile/{username}")
+    private  void pagePhotograph(@PathVariable String username, Model model, Model model1) {
+        User user =(User)userService.loadUserByUsername(username);
+        Photograph photograph = (Photograph) user.getUserDep();
+        model.addAttribute("user", user);
+        model1.addAttribute("photograph", photograph);
+        System.out.println("YOU+++++++++++++++++++++++++++++++++++++++");
+
+    }
+
+
 
 
 
